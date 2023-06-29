3122b082b839bc454bea27895535986c970d0278
Kevin Espinoza <kespinoza5@ucmerced.edu>
2023-06-29 11:42:36 -0700

    Co-authored-by: RaulLuis22 <RaulLuis22@users.noreply.github.com>

--- material.inc.php
+++ material.inc.php
@@ -32,6 +32,48 @@ $this->card_types = array(
 
 */
 
+// $this->card_types = array(
+//   'green'
+// )
 
+$this->tiles = array();
 
+foreach (range(0,11) as $i) 
+{
+    $this->tiles[$i] = array( 
+      'color' => 'blue',
+      'sprite_position' => 0
+    );
+}
 
+foreach (range(12, 23) as $i) 
+{
+    $this->tiles[$i] = array( 
+      'color' => 'red',
+      'sprite_position' => 1
+   );
+}
+
+foreach (range(24,35) as $i) 
+{
+    $this->tiles[$i] = array( 
+      'color' => 'gold',
+      'sprite_position' => 2
+     );
+}
+
+foreach (range(36,47) as $i) 
+{
+    $this->tiles[$i] = array( 
+      'color' => 'white',
+      'sprite_position' => 3
+    );
+}
+
+foreach (range(48,53) as $i) 
+{
+    $this->tiles[$i] = array( 
+      'color' => 'green',
+      'sprite_position' => 4
+    );
+}
