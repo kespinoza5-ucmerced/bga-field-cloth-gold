8e7638569f6443ba315eb39a92cdac2caf620059
Kevin Espinoza <kespinoza5@ucmerced.edu>
2024-01-03 23:44:55 -0800

    Implement js function to obtain tile color from ID

--- modules/js/utility.js
+++ modules/js/utility.js
@@ -15,4 +15,31 @@ function getTileUniqueType(color) {
     return -1;
 }
 
+function getTileTypeFromId( tile_id ) {
+    // blue
+    if (tile_id >=0 && tile_id <= 11) {
+        return 1
+    }
+
+    // red
+    if (tile_id >= 12 && tile_id <= 23) {
+        return 2
+    }
+
+    // gold
+    if (tile_id >= 24 && tile_id <= 35) {
+        return 3
+    }
+
+    // white
+    if (tile_id >= 36 && tile_id <= 47) {
+        return 4
+    }
+
+    // green
+    if (tile_id >= 48 && tile_id <= 53) {
+        return 5
+    }
+}
+
 module.exports = { getTileUniqueType }
